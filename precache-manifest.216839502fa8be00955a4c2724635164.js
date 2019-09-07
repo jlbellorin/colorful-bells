f6402e45bd332e49ce2d17345993b67497170e47
José Bellorín <jlbellorin10@gmail.com>
2019-09-07 03:46:21 -0400

    Updates

---
+++ precache-manifest.216839502fa8be00955a4c2724635164.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "274392b7cfb7030d2af8",
+    "url": "/colorful-bells/static/css/main.ed353336.chunk.css"
+  },
+  {
+    "revision": "274392b7cfb7030d2af8",
+    "url": "/colorful-bells/static/js/main.7c269b3c.chunk.js"
+  },
+  {
+    "revision": "c4ffb4b1980a1055618c",
+    "url": "/colorful-bells/static/js/runtime~main.0a8b068b.js"
+  },
+  {
+    "revision": "1e15d07c2887abd29b47",
+    "url": "/colorful-bells/static/css/2.6b240ca4.chunk.css"
+  },
+  {
+    "revision": "1e15d07c2887abd29b47",
+    "url": "/colorful-bells/static/js/2.c439ba46.chunk.js"
+  },
+  {
+    "revision": "04026feb5534c1cb841ee86c52d93c76",
+    "url": "/colorful-bells/static/media/bg.04026feb.png"
+  },
+  {
+    "revision": "f7b82c89e544fa6167dcb5be5d0117ca",
+    "url": "/colorful-bells/index.html"
+  }
+];
